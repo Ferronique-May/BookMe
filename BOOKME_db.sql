d8037f40ab9085daca39dd7e6e3e063d3ee5e0fe
Ferronique May <ferronique@bbd.co.za>
2020-03-03 10:05:14 +0200

    changes made to tables and constraints

--- BOOKME_db.sql
+++ BOOKME_db.sql
@@ -1,90 +1,153 @@
-CREATE DATABASE BOOKME;
+--CREATE DATABASE
+CREATE DATABASE BookMe;
 GO
 
+--CREATE TABLES
 CREATE TABLE Patients (
-    PatientID INT NOT NULL PRIMARY KEY,
-    FullName VARCHAR(80) NOT NULL
+    PatientID VARCHAR(13) NOT NULL PRIMARY KEY,
+    FullName VARCHAR(80) NOT NULL,
+	AdditionalInfoID INT NOT NULL
 );
 GO
+
 
 CREATE TABLE Doctors (
-    DoctorID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    FullName VARCHAR(80) NOT NULL
+    DoctorID VARCHAR(13) NOT NULL PRIMARY KEY,
+    FullName VARCHAR(80) NOT NULL,
+	AdditionalInfoID INT NOT NULL
 );
 GO
 
-CREATE TABLE PatientDetails(
-	PatientDetailsID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	Email VARCHAR(80) NOT NULL,
-	PhoneNumber INT NOT NULL,
-	PatientID INT NOT NULL,
-	PatientPassword VARCHAR(150) NOT NULL
-);
-GO
 
-CREATE TABLE DoctorDetails(
-	DoctorDetailsID INT NOT NULL PRIMARY KEY,
-	DoctorID INT NOT NULL,
+CREATE TABLE AdditionalInfo(
+	AdditionalInfoID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Email VARCHAR(80) NOT NULL,
-	PhoneNumber INT NOT NULL,
-	DoctorPassword VARCHAR(150) NOT NULL
+	PhoneNumber VARCHAR(10) NOT NULL,
+	PasswordHash VARCHAR(150) NOT NULL 
 );
 GO
 
 CREATE TABLE Appointments(
-	AppointentID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	AppointmentID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	AppointmentDateTime DATETIME NOT NULL,
-	PatientID INT NOT NULL,
+	DoctorID VARCHAR(13) NOT NULL,
 	AppointmentDescription VARCHAR(200) NULL
 );
 GO
 
 CREATE TABLE PatientAppointments(
 	AppointmentID INT NOT NULL,
-	PatientID INT NOT NULL,
-	DoctorID INT NOT NULL
+	PatientID VARCHAR(13) NOT NULL
 );
+GO
+
+--MAKING A PRIMARY KEY FOR PATIENTAPPOINTMENTS
+ALTER TABLE PatientAppointments ADD CONSTRAINT PK_PatientAppointments_AppointmentID_PatientID PRIMARY KEY (AppointmentID, PatientID);
 GO
 
 CREATE TABLE MedicalAidNumber(
 	MedicalAidID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	PatientID INT NOT NULL,
+	PatientID VARCHAR(13) NOT NULL,
 	MemberNumber INT NOT NULL
 );
 GO
 
 CREATE TABLE AlternativePayment(
 	PaymentID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	PatientID INT NOT NULL,
+	PatientID VARCHAR(13) NOT NULL,
 	CardNumber VARCHAR(150) NOT NULL,
 	ExpiryDate VARCHAR(150) NOT NULL,
 	CVV VARCHAR(150) NOT NULL
 );
 GO
 
---ADD FOREIGN KEYS\
-ALTER TABLE PatientAppointments ADD CONSTRAINT PatientAppointments_AppointmentID_DoctorID_PatientID_PRI PRIMARY KEY (AppointmentID,DoctorID,PatientID)
-ALTER TABLE AlternativePayment ADD CONSTRAINT AlternativePayment_PatientID_FK FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
-ALTER TABLE MedicalAidNumber ADD CONSTRAINT MedicalAidNumber_PatientID_FK FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
-ALTER TABLE PatientAppointments ADD CONSTRAINT PatientAppointments_PatientID_FK FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
-ALTER TABLE PatientAppointments ADD CONSTRAINT PatientAppointments_DoctorID_FK FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID);
-ALTER TABLE PatientAppointments ADD CONSTRAINT PatientAppointments_AppointmentID_FK FOREIGN KEY (AppointmentID) REFERENCES Appointments(AppointmentID);
-ALTER TABLE Appointments ADD CONSTRAINT Appointments_PatientID_FK FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
-ALTER TABLE DoctorDetails ADD CONSTRAINT DoctorDetails_DoctorID_FK FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID);
-ALTER TABLE PatientDetails ADD CONSTRAINT PatientDetails_DoctorID_FK FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
+
+
+--ADD FOREIGN KEYS
+ALTER TABLE Patients ADD CONSTRAINT FK_Patients_AdditionalInfoID FOREIGN KEY (AdditionalInfoID) REFERENCES AdditionalInfo(AdditionalInfoID);
+ALTER TABLE Doctors ADD CONSTRAINT FK_Doctors_AdditionalInfoID FOREIGN KEY (AdditionalInfoID) REFERENCES AdditionalInfo(AdditionalInfoID);
+ALTER TABLE Appointments ADD CONSTRAINT FK_Appointments_DoctorID FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID);
+ALTER TABLE PatientAppointments ADD CONSTRAINT FK_PatientAppointments_AppointmentID  FOREIGN KEY (AppointmentID) REFERENCES Appointments(AppointmentID);
+ALTER TABLE PatientAppointments ADD CONSTRAINT FK_PatientAppointments_PatientID  FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
+ALTER TABLE MedicalAidNumber ADD CONSTRAINT FK_MedicalAidNumber_PatientID  FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
+ALTER TABLE AlternativePayment ADD CONSTRAINT FK_AlternativePayment_PatientID  FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
 GO
 
 --UNIQUE CONSTRAINTS
-ALTER TABLE PatientDetails ADD CONSTRAINT UNIQUE_CK_PATIENTDETAILS_EMAIL UNIQUE (Email)
-ALTER TABLE DoctorDetails ADD CONSTRAINT UNIQUE_CK_DOCTORDETAILS_EMAIL UNIQUE (Email)
-ALTER TABLE Patients ADD CONSTRAINT UNIQUE_CK_PATIENTS_PATIENTID UNIQUE (PatientID)
-ALTER TABLE DOCTORS ADD CONSTRAINT UNIQUE_CK_DOCTORS_DOCTORID UNIQUE (DoctorsID)
+ALTER TABLE AdditionalInfo ADD CONSTRAINT UNIQUE_AdditionalInfo_Email UNIQUE(Email);
+ALTER TABLE Patients ADD CONSTRAINT UNIQUE_Patients_PatientID UNIQUE (PatientID);
+ALTER TABLE Doctors ADD CONSTRAINT UNIQUE_Doctors_DoctorID UNIQUE (DoctorID);
 GO
 
 --CHECK CONSTRAINTS
-ALTER TABLE PatientDetails
-    ADD CONSTRAINT PATIENTDETAILS_PHONENUMBER_CK CHECK (LEN(password) = 10)
+ALTER TABLE AdditionalInfo ADD CONSTRAINT CK_AdditionalInfo_PhoneNumber CHECK (LEN(PhoneNumber) = 10);
+ALTER TABLE Patients ADD CONSTRAINT CK_Patients_PatientID CHECK (LEN(PatientID) = 13);
+GO
 
-CREATE PROCEDURE InsertUser{
-	@PatientID
-}
+--STORED PROCEDURES TO CREATE USERS
+CREATE PROCEDURE InsertAdditionalInfo(@email VARCHAR(80), @phonenumber VARCHAR(10), @password VARCHAR(50))
+	AS
+	BEGIN TRY
+		INSERT INTO AdditionalInfo (Email,PhoneNumber,PasswordHash) VALUES (@email, @phonenumber, @password)
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_STATE() AS ErrorState,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH;
+GO
+
+CREATE PROCEDURE InsertPatient(@patientID VARCHAR(13), @fullname VARCHAR(80), @additionalInfoID INT)
+	AS
+	BEGIN TRY
+		INSERT INTO Patients(PatientID, FullName, AdditionalInfoID) VALUES (@patientID, @fullname, @additionalInfoID);
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_STATE() AS ErrorState,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH;
+GO
+
+CREATE PROCEDURE InsertDoctor(@doctorID VARCHAR(13), @fullname VARCHAR(80), @additionalInfoID INT)
+	AS
+	BEGIN TRY
+		INSERT INTO Doctors(DoctorID, FullName, AdditionalInfoID) VALUES (@doctorID, @fullname, @additionalInfoID);
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_STATE() AS ErrorState,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH;
+GO
+
+--STORED PROCEDURE TO ADD APPOINTMENTS
+
+CREATE PROCEDURE CreateAppointment(@appointmentDateTime DATETIME, @doctorID VARCHAR(13),@appointmentDescription VARCHAR(200))
+	AS
+	BEGIN TRY
+		INSERT INTO Appointments(AppointmentDateTime, DoctorID, AppointmentDescription) VALUES (@appointmentDateTime, @doctorID, @appointmentDescription);
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_STATE() AS ErrorState,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH;
+GO
+
