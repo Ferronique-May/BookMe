c53c8b912cfc3238dd2493165c09fd4652d2e607
Malibongwe Mkhwanazi <malibongwem@bbd.co.za>
2020-03-03 11:43:58 +0200

    now

---
+++ Server/Register.js
@@ -0,0 +1,13 @@
+const RegisterForm = document.querySelector('#RegisterForm');
+const FullName = document.querySelector('#FullName');
+const LastName = document.querySelector('#LastName');
+const Email = document.querySelector('#Email');
+const Password = document.querySelector('#Password');
+const ConfirmPassword = document.querySelector('#ConfirmPassword');
+
+RegisterForm.addEventListener('Submit', onsubmit);
+
+if (FullName.value === '' || LastName.value)
+{
+    
+}
