62d1ead5ec823242c647101a7314b5a67e3d8a7b
Waseem Ahmed <waseema@bbd.co.za>
2020-03-04 12:05:29 +0200

    fixed

--- routes/register.js
+++ routes/register.js
@@ -8,7 +8,7 @@ router.use(bodyParser.json());
 
 const dbConfig = {
   driver: 'msnodesqlv8',
-  connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost\\SQLExpress};Database={test};Trusted_Connection={yes};'
+  connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost\\SQLExpress};Database={BookMeDB};Trusted_Connection={yes};'
   }
 
 router.get('/', function(req, res) {
@@ -25,7 +25,7 @@ router.post('/' ,function(request, response){
     const PhoneNumber = request.body.PhoneNumber;
     const IDNumber = request.body.IDNumber;
     const ConfirmPassword = request.body.ConfirmPassword;
-   console.log("Testing......")
+   
     if (Password && FullName && PhoneNumber && Email && IDNumber && ConfirmPassword){
         sql.connect(dbConfig, function(err){
         if(err){
@@ -34,14 +34,15 @@ router.post('/' ,function(request, response){
             sql.close();
         }
         else {  
-            console.log("connected");                     
+            console.log("connected");    
+            console.log(FullName);                 
             const request = new sql.Request();   
-            request.input('fullname', sql.VarChar, FullName);
-            request.input('password', sql.VarChar, Password);       
-            request.input('email', sql.VarChar, Email);       
-            request.input('phonenumber', sql.VarChar, PhoneNumber);
-            request.input('idnumber', sql.VarChar, IDNumber);    
-            request.query("INSERT INTO Patients (PatientID, FullName, Email, PhoneNumber, PasswordHash) VALUES (@idnumber, @fullname, @email, @phonenumber, @password);",function(error, results){
+            request.input('FullName', sql.VarChar, FullName);
+            request.input('Password', sql.VarChar, Password);       
+            request.input('Email', sql.VarChar, Email);       
+            request.input('PhoneNumber', sql.VarChar, PhoneNumber);
+            request.input('IDNumber', sql.VarChar, IDNumber);    
+            request.query("INSERT INTO Patients (PatientID, FullName, Email, PhoneNumber, PasswordHash) VALUES (@IDNumber, @FullName, @Email, @PhoneNumber, @Password);",function(error, results){
                 if(error){
                     console.log("not inserted into database");
                     console.log(request.body.FullName);
