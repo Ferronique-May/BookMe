f6bbb9ea68ee25c2214dc74a7d85c077f40728c5
Waseem Ahmed <waseema@bbd.co.za>
2020-03-04 11:43:46 +0200

    form not submitting to db

--- Server/Login.js
+++
@@ -1,5 +0,0 @@
-const Email = document.querySelector('#Email');
-const Password = document.querySelector('#Password');
-const LoginForm = document.querySelector('#LoginForm');
-
-LoginForm.addEventListener('')
