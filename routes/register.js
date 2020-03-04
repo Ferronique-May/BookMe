0b77ed951d0712521c202b1a428ecde90c47fcfe
Concellia <tiyiselani.rhangani@gmail.com>
2020-03-04 14:17:01 +0200

    stored procedure

--- routes/register.js
+++ routes/register.js
@@ -8,61 +8,57 @@ router.use(bodyParser.json());
 
 const dbConfig = {
   driver: 'msnodesqlv8',
-  connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost\\SQLExpress};Database={test};Trusted_Connection={yes};'
+  connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost\\SQLExpress};Database={BookMeDB};Trusted_Connection={yes};'
   }
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname , '../Client/Register.html'));
-    console.log(__dirname);
-    console.log("Test get....")
 });
+router.post("/", async (req, res) => {
+    let data = {};
+    try {
+      const requested = req.body;
+      console.log(requested.IDNumber)
+      await InsertUpdateDeletePatient('INSERT',
+      requested.IDNumber,
+      requested.FullName,
+      requested.Email,
+      requested.PhoneNumber,
+      requested.Password
+      )
+      data.success = true;
+    } catch (e) {
+      data.success = false;
+    } finally {
+      res.setHeader("content-type", "application/json");
+      res.redirect("/");
+    }
+  });
 
-console.log("hereeeee");
-router.post('/' ,function(request, response){
-    const FullName = request.body.FullName;
-    const Password = request.body.Password;
-    const Email = request.body.Email;
-    const PhoneNumber = request.body.PhoneNumber;
-    const IDNumber = request.body.IDNumber;
-    const ConfirmPassword = request.body.ConfirmPassword;
-   console.log("Testing......")
-    if (Password && FullName && PhoneNumber && Email && IDNumber && ConfirmPassword){
-        sql.connect(dbConfig, function(err){
+function InsertUpdateDeletePatient(statementType,PatientID,fullname,email,phonenumber,password){
+    const connection = new sql.ConnectionPool(dbConfig);
+    const request = new sql.Request(connection);
+    request.input('StatemetType', sql.VarChar, statementType);
+    request.input('PatientID', sql.VarChar, PatientID);
+    request.input('Fullname', sql.VarChar, fullname);       
+    request.input('Email', sql.VarChar, email);       
+    request.input('Phonenumber', sql.VarChar, phonenumber);
+    request.input('Password', sql.VarChar, password); 
+    connection.connect(function(err){
         if(err){
-            console.log("Error while connecting database :- " + err);
-            response.send(err);
-            sql.close();
+            console.log(err)
+            return    
         }
-        else {  
-            console.log("connected");                     
-            const request = new sql.Request();   
-            request.input('fullname', sql.VarChar, FullName);
-            request.input('password', sql.VarChar, Password);       
-            request.input('email', sql.VarChar, Email);       
-            request.input('phonenumber', sql.VarChar, PhoneNumber);
-            request.input('idnumber', sql.VarChar, IDNumber);    
-            request.query("INSERT INTO Patients (PatientID, FullName, Email, PhoneNumber, PasswordHash) VALUES (@idnumber, @fullname, @email, @phonenumber, @password);",function(error, results){
-                if(error){
-                    console.log("not inserted into database");
-                    console.log(request.body.FullName);
-                    response.send("not added to database");
-                }
-                else{
-                    console.log("inserted to database");
-                    //response.send("successfully added to database");
-                    response.redirect("/");
-                }
-                sql.close();    
-            });            
-         }
-    });
-    }
-    else{
-        response.send('Please enter Username and Password!');
-		response.end();
+        request.query("EXEC SP_InsertUpdateDeletePatient @StatemetType,@PatientID,@Fullname,@Email,@PhoneNumber,@Password",function(err,data){
+            if(err){
+                console.log('FAIL......')
+            }else{
+                console.log(data.recordset)
+            } 
+            connection.close();
+        })
 
-    }
-});
-
+    })
+}
 
 module.exports = router;
